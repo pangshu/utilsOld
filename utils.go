9289f9289c6fca8faf0eb97f89712417574a6da7
pangshu <ipqqqug@qq.com>
2021-10-18 16:32:09 +0800

    [修改]引入库资源

--- utils.go
+++ utils.go
@@ -3,11 +3,23 @@ package utils
 import (
 	DuduConvert "github.com/pangshu/utils/Convert"
 	DuduDate "github.com/pangshu/utils/Date"
+	DuduFile "github.com/pangshu/utils/File"
+	DuduHtml "github.com/pangshu/utils/Html"
+	DuduLog "github.com/pangshu/utils/Log"
+	DuduNet "github.com/pangshu/utils/Net"
 	DuduNumber "github.com/pangshu/utils/Number"
+	DuduString "github.com/pangshu/utils/String"
+	DuduUrl "github.com/pangshu/utils/Url"
 )
 
 var (
-	Number DuduNumber.Number
 	Convert DuduConvert.Convert
 	Date DuduDate.Date
+	File DuduFile.File
+	Html DuduHtml.Html
+	Log  DuduLog.Log
+	Net  DuduNet.Net
+	Number DuduNumber.Number
+	String DuduString.String
+	Url DuduUrl.Url
 )
